259bb149684390c2b91b73de31edd23e6f1c9c40
Mijael30100 <mijaelkoro@gmail.com>
2024-05-29 17:30:07 -0300

    Eliminados los print

--- demo/src/main/java/ModelosySimulaciones/demo/model/EnigmaMachine.java
+++ demo/src/main/java/ModelosySimulaciones/demo/model/EnigmaMachine.java
@@ -14,36 +14,25 @@ public class EnigmaMachine {
         for (char c : plaintext.toCharArray()) {
             if (Character.isLetter(c)) {
                 c = Character.toUpperCase(c);
-                System.out.println(c);
                 stepRotors();
                 c = passThroughRotorsForward(c);
-                System.out.println(c);
                 c = reflector.reflect(c);
-                System.out.println(c);
                 c = passThroughRotorsBackward(c);
-                System.out.println(c);
             }
             ciphertext.append(c);
         }
         return ciphertext.toString();
     }
 
-    private void stepRotors() {
-        System.out.println("Rotor positions before stepping: " + rotors[0].getPosition() + ", " + rotors[1].getPosition() + ", " + rotors[2].getPosition());
-    
+    private void stepRotors() {    
         if (rotors[1].atNotch() && rotors[0].atNotch() ) {
             rotors[2].step();
-            System.out.println("Stepped rotor 3");
         }
         if (rotors[0].atNotch()) {
             rotors[1].step();
-            System.out.println("Stepped rotor 2");
         }
         rotors[0].step();
-        System.out.println("Stepped rotor 1");
-    
-        System.out.println("Rotor positions after stepping: " + rotors[0].getPosition() + ", " + rotors[1].getPosition() + ", " + rotors[2].getPosition());
-    }
+        }
     
 
     private char passThroughRotorsForward(char c) {
