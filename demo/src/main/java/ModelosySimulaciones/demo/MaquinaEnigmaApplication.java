7d3647a368847545b1c00929e209743654fe757f
Mijael30100 <mijaelkoro@gmail.com>
2024-06-07 10:50:09 -0300

    Run Browser

--- demo/src/main/java/ModelosySimulaciones/demo/MaquinaEnigmaApplication.java
+++ demo/src/main/java/ModelosySimulaciones/demo/MaquinaEnigmaApplication.java
@@ -2,12 +2,42 @@ package ModelosySimulaciones.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class MaquinaEnigmaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaquinaEnigmaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MaquinaEnigmaApplication.class, args);
+    }
 
+	@EventListener({ApplicationReadyEvent.class})
+	void applicationReadyEvent() {
+		System.out.println("Application started ... launching browser now");
+		browse("http://localhost:8080/enigma");
+	}
+	
+	public static void browse(String url) {
+		if(Desktop.isDesktopSupported()){
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}else{
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
